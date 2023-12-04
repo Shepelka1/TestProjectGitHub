e8fecdfad9afdf9beae549ffb4d4cfc85b0331cc
akazakov21 <131651247+akazakov21@users.noreply.github.com>
2023-12-04 08:31:18 +0200

    Wow amazing branch, very epic (#3)
    Co-authored-by: suck ma cock <kaoacj>

--- Library/Class1.cs
+++ Library/Class1.cs
@@ -4,6 +4,19 @@ namespace Library
 {
     public class Class1
     {
+
+        public int ivanP1;
+        public int ivanP2;
+        public int ivanP3;
+
+        public int Const(int IvanP1, int IvanP2, int IvanP3)
+        {
+            this.ivanP1 = IvanP1;
+            this.ivanP2 = IvanP2;
+            this.ivanP3 = IvanP3;
+        }
+        
+
         public int acho1;
         public int acho2;
         public int acho3;
--- Library/Class2.cs
+++ Library/Class2.cs
@@ -4,6 +4,15 @@ namespace Library
 {
     public class Class2
     {
+        public int ivanP4;
+        public int ivanP5;
+        public int ivanP6;
+
+        public int Const(int IvanP4, int IvanP5, int IvanP6)
+        {
+            this.ivanP4 = IvanP4;
+            this.ivanP5 = IvanP5;
+            this.ivanP6 = IvanP6;
         public int test1;
         public int test2;
         public int test3;
@@ -18,6 +27,7 @@ namespace Library
             this.CoolISBN = CoolISBN;
             this.CoolAuthor = CoolAuthor;
             this.CreationDate = CreationDate;
+ 
         }
     }
 }
