0146d0fb245560dd20e31a231bb3bae820e27947
IvanParaskevov <131652703+IvanParaskevov@users.noreply.github.com>
2023-12-04 08:19:21 +0200

    Cool classes added (#2)
    Co-authored-by: IvanS <ishepelev898@gmail.com>

--- Library/Class2.cs
+++ Library/Class2.cs
@@ -4,5 +4,14 @@ namespace Library
 {
     public class Class2
     {
+        public string CoolISBN { get; set; }
+        public string CoolAuthor { get; set; }
+        public DateTime CreationDate { get; set; }
+        public Class2(string CoolISBN, string CoolAuthor, DateTime CreationDate)
+        {
+            this.CoolISBN = CoolISBN;
+            this.CoolAuthor = CoolAuthor;
+            this.CreationDate = CreationDate;
+        }
     }
 }
--- Library/Class3.cs
+++ Library/Class3.cs
@@ -4,5 +4,14 @@ namespace Library
 {
     public class Class3
     {
+        public string CoolLibraryName { get; set; }
+        public string CoolAdress { get; set; }
+        public decimal Revenue { get; set; }
+        public Class3(string CoolLibraryName, string CoolAdress, decimal Revenue)
+        {
+            this.CoolLibraryName = CoolLibraryName;
+            this.CoolAdress = CoolAdress;
+            this.Revenue = Revenue;
+        }
     }
 }
