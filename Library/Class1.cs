b52820acac5f3b02ff380fe24caf6dff651351b2
IvanS <ishepelev898@gmail.com>
2023-12-04 08:06:22 +0200

    Cool classes added

--- Library/Class1.cs
+++ Library/Class1.cs
@@ -4,5 +4,14 @@ namespace Library
 {
     public class Class1
     {
+        public string CoolName { get; set; }
+        public int CoolYear { get; set; }
+        public DateTime FavDateTime { get; set; }
+        public Class1(string CoolName, int CoolYear, DateTime FavDateTime)
+        {
+            this.CoolName = CoolName;
+            this.CoolYear = CoolYear;
+            this.FavDateTime = FavDateTime;
+        }
     }
 }
